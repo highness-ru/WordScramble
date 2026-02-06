8d106711e18584e52a8c04b5fa09943079b3dd3d
highness-ru <highness.ru@gmail.com>
2026-02-06 18:21:02 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Анна Герман on 06.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
